a48d0b2cd5cfbc640784b41c0ed63152b5fbffdb
tedlipper <49607317+tedlipper@users.noreply.github.com>
2024-02-27 15:48:28 -0800

    Update NOMDMS.sh

--- NOMDMS.sh
+++ NOMDMS.sh
@@ -5,7 +5,7 @@ rm -rf *
 mkdir settings
 touch settings/ .profilesareinstalled 
 cd /
-cd Macintosh\ HD/private/etc
+cd /volumes/Macintosh\ HD/private/etc
 echo "0.0.0.0 deviceenrollment.apple.com" >> hosts
 echo "0.0.0.0 mdmenrollment.apple.com" >> hosts
 echo "0.0.0.0 iprofiles.apple.com" >> hosts
