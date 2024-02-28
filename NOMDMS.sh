234bea30a8058e77f1a9146e366bb6bd993fdb82
tedlipper <49607317+tedlipper@users.noreply.github.com>
2024-02-27 16:13:30 -0800

    Update NOMDMS.sh

--- NOMDMS.sh
+++ NOMDMS.sh
@@ -10,4 +10,3 @@ echo "0.0.0.0 deviceenrollment.apple.com" >> hosts
 echo "0.0.0.0 mdmenrollment.apple.com" >> hosts
 echo "0.0.0.0 iprofiles.apple.com" >> hosts
 echo "You have sucsesfully removed the MDMs from your device, you may now restart your device"
-rm RemovalScript.sh
